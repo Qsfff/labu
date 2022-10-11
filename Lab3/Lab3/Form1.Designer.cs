231dbe4d5c9f0107726d4af4304c39f1e7f5cfc8
Qsfff <112318227+Qsfff@users.noreply.github.com>
2022-10-11 19:40:20 +0700

    Add files via upload

---
+++ Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Lab3
+{
+    partial class binConverter
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.in1 = new System.Windows.Forms.TextBox();
+            this.res1 = new System.Windows.Forms.Label();
+            this.res2 = new System.Windows.Forms.Label();
+            this.in2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.in3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.res4 = new System.Windows.Forms.Label();
+            this.in4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.res3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b1.Location = new System.Drawing.Point(758, 49);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(82, 64);
+            this.b1.TabIndex = 7;
+            this.b1.Text = "Do it!";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b2.Location = new System.Drawing.Point(758, 147);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(82, 64);
+            this.b2.TabIndex = 8;
+            this.b2.Text = "Do it!";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b3.Location = new System.Drawing.Point(758, 254);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(82, 64);
+            this.b3.TabIndex = 9;
+            this.b3.Text = "Do it!";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b4.Location = new System.Drawing.Point(758, 361);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(82, 64);
+            this.b4.TabIndex = 10;
+            this.b4.Text = "Do it!";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Convert to binary";
+            // 
+            // in1
+            // 
+            this.in1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.in1.Location = new System.Drawing.Point(248, 49);
+            this.in1.Name = "in1";
+            this.in1.Size = new System.Drawing.Size(465, 24);
+            this.in1.TabIndex = 12;
+            // 
+            // res1
+            // 
+            this.res1.AutoSize = true;
+            this.res1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res1.Location = new System.Drawing.Point(245, 95);
+            this.res1.Name = "res1";
+            this.res1.Size = new System.Drawing.Size(58, 18);
+            this.res1.TabIndex = 13;
+            this.res1.Text = "Result: ";
+            // 
+            // res2
+            // 
+            this.res2.AutoSize = true;
+            this.res2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res2.Location = new System.Drawing.Point(245, 193);
+            this.res2.Name = "res2";
+            this.res2.Size = new System.Drawing.Size(58, 18);
+            this.res2.TabIndex = 16;
+            this.res2.Text = "Result: ";
+            // 
+            // in2
+            // 
+            this.in2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.in2.Location = new System.Drawing.Point(248, 145);
+            this.in2.Name = "in2";
+            this.in2.Size = new System.Drawing.Size(465, 24);
+            this.in2.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Convert to decimal";
+            // 
+            // in3
+            // 
+            this.in3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.in3.Location = new System.Drawing.Point(248, 252);
+            this.in3.Name = "in3";
+            this.in3.Size = new System.Drawing.Size(465, 24);
+            this.in3.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 18);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Convert to binary";
+            // 
+            // res4
+            // 
+            this.res4.AutoSize = true;
+            this.res4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res4.Location = new System.Drawing.Point(245, 405);
+            this.res4.Name = "res4";
+            this.res4.Size = new System.Drawing.Size(58, 18);
+            this.res4.TabIndex = 22;
+            this.res4.Text = "Result: ";
+            // 
+            // in4
+            // 
+            this.in4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.in4.Location = new System.Drawing.Point(248, 359);
+            this.in4.Name = "in4";
+            this.in4.Size = new System.Drawing.Size(465, 24);
+            this.in4.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(25, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 18);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Convert to decimal";
+            // 
+            // res3
+            // 
+            this.res3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res3.Location = new System.Drawing.Point(248, 294);
+            this.res3.Name = "res3";
+            this.res3.Size = new System.Drawing.Size(465, 24);
+            this.res3.TabIndex = 23;
+            this.res3.Text = "Result: ";
+            // 
+            // binConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 492);
+            this.Controls.Add(this.res3);
+            this.Controls.Add(this.res4);
+            this.Controls.Add(this.in4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.in3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.res2);
+            this.Controls.Add(this.in2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.res1);
+            this.Controls.Add(this.in1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "binConverter";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "binConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox in1;
+        private System.Windows.Forms.Label res1;
+        private System.Windows.Forms.Label res2;
+        private System.Windows.Forms.TextBox in2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox in3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label res4;
+        private System.Windows.Forms.TextBox in4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox res3;
+    }
+}
+
---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class binConverter : Form
+    {
+        static int[] dual10v2(int n)
+        {
+            int[] a = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 15; i >= 0; i--)
+            {
+                a[i] = n & 1;
+                n = n >> 1;
+            }
+            return a;
+        }
+        static int[] _10v2(int n)
+        {
+            int[] a = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 7; i >= 0; i--)
+            {
+                a[i] = n & 1;
+                n = n >> 1;
+            }
+            return a;
+        }
+        static int _2v10(int[] a)
+        {
+            int n = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                n = (int)(n + (a[i] * Math.Pow(2, (7 - i))));
+            }
+            //if (a[0] == 1) n = -128 + n;
+            return n;
+        }
+        static float mant2v10(int[] a)
+        {
+            float n = 0;
+            for (int i = 23; i >= 0; i--)
+            {
+                n = (float)(n + (a[i] * Math.Pow(2, -i - 1)));
+            }
+            return n;
+        }
+        static float ce2v10(int[] a)
+        {
+            float n = 0;
+            for (int i = 23; i >= 0; i--)
+            {
+                n = (float)(n + (a[i] * Math.Pow(2, (23-i))));
+            }
+            return n;
+        }
+        static int[] manttoarr(string s)
+        {
+            int[] a = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < 23; i++)
+            {
+                a[i] = int.Parse(char.ToString(s[i]));
+            }
+            return a;
+        }
+        static int[] _10v2(byte n)
+        {
+            int[] a = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 7; i >= 0; i--)
+            {
+                a[i] = n & 1;
+                n = (byte)(n >> 1);
+            }
+            return a;
+        }
+        static string arrtostr(int[] a)
+        {
+            string s = "";
+            foreach (int i in a)
+            {
+                s = s + i;
+            }
+            return s;
+        }
+        static byte byte2v10(int[] a)
+        {
+            byte n = 0;
+            for (int i = 7; i >= 0; i--)
+            {
+                n = (byte)(n + (a[i] * Math.Pow(2, (7 - i))));
+            }
+            return n;
+        }
+        static int[] strtoarr(string s)
+        {
+            int[] a = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < 8; i++)
+            {
+                a[i] = int.Parse(char.ToString(s[i]));
+            }
+            return a;
+        }
+        public binConverter()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b1.Click += new EventHandler(b1_Click);
+            Controls.Add(b1);
+            b2.Click += new EventHandler(b2_Click);
+            Controls.Add(b2);
+            b3.Click += new EventHandler(b3_Click);
+            Controls.Add(b3);
+            b4.Click += new EventHandler(b4_Click);
+            Controls.Add(b4);
+        }
+        private void b1_Click(object b1, EventArgs e)
+        {
+            string tmp = "";
+            byte[] a = BitConverter.GetBytes(Single.Parse(in1.Text));
+            for (int i = 3; i >= 0; i--)
+            {
+                tmp += arrtostr(_10v2(a[i]));
+            }
+            res1.Text = "Result: " + tmp.Substring(0, 1) + " " + tmp.Substring(1, 8) + " " + tmp.Substring(9);
+        }
+        private void b2_Click(object b2, EventArgs e)
+        {
+            res2.Text = "Result: ";
+            int[][] a = new int[4][];
+            for (int i = 0; i < 4; i++)
+            {
+                a[i] = new int[8];
+                for (int j = 0; j < 8; j++)
+                {
+                    a[i][j] = int.Parse(in2.Text.Substring((i * 8) + j, 1));
+                }
+            }
+            byte[] b = new byte[4];
+            for (int i = 3; i >= 0; i--)
+            {
+                b[i] = byte2v10(a[3-i]);
+            }
+            res2.Text += BitConverter.ToSingle(b, 0);
+        }
+        private void b3_Click(object b3, EventArgs e)
+        {
+            res3.Text = "Result: ";
+            double d = double.Parse(in3.Text);
+            if (d != 0)
+            {
+                string z = d < 0 ? "1" : "0";
+                d = Math.Abs(d);
+                string c = arrtostr(dual10v2((int)d));
+                while (c.Length != 0 && c[0] == '0')
+                {
+                    c = c.Remove(0, 1);
+                }
+                d = Math.Abs(d - (int)d);
+                string dr = "";
+                for (int i = 0; i < 24; i++)
+                {
+                    d *= 2;
+                    if (d > 1)
+                    {
+                        dr += "1";
+                        d -= 1;
+                    }
+                    else dr += "0";
+                }
+                string zapas = "";
+                for (int i = 0; i < 24; i++)
+                {
+                    d *= 2;
+                    if (d > 1)
+                    {
+                        zapas += "1";
+                        d -= 1;
+                    }
+                    else zapas += "0";
+                }
+                string mant = "";
+                int exp = 0;
+                if (c != "")
+                {
+                    c = c.Remove(0, 1);
+                    mant = c != "" ? c + dr : dr;
+                    exp = c.Length;
+                    if (mant.Length > 23) mant = mant.Remove(23);
+                }
+                else
+                {
+                    while (dr[0] == '0')
+                    {
+                        dr = dr.Remove(0, 1);
+                        exp -= 1;
+                    }
+                    dr = dr.Remove(0, 1);
+                    exp -= 1;
+                    mant = dr + zapas;
+                    if (mant.Length > 23) mant = mant.Remove(23);
+                }
+                string ex = arrtostr(_10v2(exp-129));
+                res3.Text += (z + ex + mant).Substring(0, 1) /*+ " "*/ + (z + ex + mant).Substring(1, 8) /*+ " "*/ + (z + ex + mant).Substring(9);
+            }
+            else res3.Text = "Result: 0 10000000 00000000000000000000000";
+        }
+        private void b4_Click(object b4, EventArgs e)
+        {
+            string z = in4.Text.Substring(0, 1);
+            string ex = in4.Text.Substring(1, 8);
+            string m = in4.Text.Substring(9);
+            int exp = _2v10(strtoarr(ex)) - 127;
+            string c = "1";
+            float ce;
+            float d;
+            if (exp >= 0)
+            {
+                for (int i = 0; i < exp; i++)
+                {
+                    c += m.Substring(0, 1);
+                    m = m.Remove(0, 1);
+                    m += "0";
+                }
+                while (m.Length < 24) m += "0";
+                while (c.Length < 24) c = "0" + c;
+                d = mant2v10(manttoarr(m));
+                ce = ce2v10(manttoarr(c));
+            }
+            else
+            {
+                m = "1" + m;
+                exp += 1;
+                c = "";
+                d = mant2v10(manttoarr(m)) * (float)Math.Pow(2, exp);
+                ce = 0;
+            }
+            ce += d;
+            ce *= (float)Math.Pow(-1, int.Parse(z));
+            res4.Text = "Result: " + ce;
+        }
+    }
+}

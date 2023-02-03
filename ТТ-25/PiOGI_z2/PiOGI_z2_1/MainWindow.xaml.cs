bee846ff083e5b5691952c7be16bd2a271f84a10
Qsfff <112318227+Qsfff@users.noreply.github.com>
2023-02-03 11:24:47 +0700

    Add files via upload

---
+++ ТТ-25/PiOGI_z2/PiOGI_z2_1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiOGI_z2_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void plus(object sender, RoutedEventArgs e)
+        {
+            res.Content = int.Parse(tA.Text) + int.Parse(tB.Text);
+        }
+        private void negative(object sender, RoutedEventArgs e)
+        {
+            res.Content = int.Parse(tA.Text) - int.Parse(tB.Text);
+        }
+        private void positive(object sender, RoutedEventArgs e)
+        {
+            res.Content = int.Parse(tA.Text) * int.Parse(tB.Text);
+        }
+        private void annigilator(object sender, RoutedEventArgs e)
+        {
+            if (int.Parse(tB.Text) != 0)
+                res.Content = int.Parse(tA.Text) / int.Parse(tB.Text);
+            else res.Content = "NaN";
+        }
+    }
+}
---
+++ ТТ-25/PiOGI_z2/PiOGI_z2_3/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiOGI_z2_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void f1(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Blizhayshaya k solncu";
+        }
+        private void f2(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Ochen goryachaya";
+        }
+        private void f3(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Prigodna dlya zhizni";
+        }
+        private void f4(object sender, RoutedEventArgs e)
+        {
+            res.Content = "est led";
+        }
+        private void f5(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Bolshie kolca";
+        }
+        private void f6(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Bolshoy";
+        }
+        private void f7(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Uran";
+        }
+        private void f8(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Neptun";
+        }
+        private void f9(object sender, RoutedEventArgs e)
+        {
+            res.Content = "Ne planeta";
+        }
+    }
+}
